35654fe592d58ba36ee68c7b1cc518a4e77fd8f3
Yigit <braverose4237@gmail.com>
2022-04-27 03:25:40 +0200

    git ignore içine .txt eklendi ve tüm eklenen .txt verileri görünmüzor sadece girignore da oynandığı görünüyor.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
     //Doing awesome work 3
     //Awesome work 3 is done and ready to committed
     //and readt to be remote reposity
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
